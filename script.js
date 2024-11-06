eff87e45cb0e486f7abd77e599d990ab32f7b9d0
whomeidk <92547104+idontunderstandverymuch@users.noreply.github.com>
2024-11-05 23:19:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,104 +1,36 @@
-// ==UserScript==
-// @name         anti-codehs
-// @namespace    http://tampermonkey.net/
-// @version      2024-08-05
-// @description  i hate codehs
-// @author       arnaclez
-// @match        https://codehs.com/*
-// @grant        none
-// ==/UserScript==
+var textarea = document.getElementById('ace_text-input-textarea');
+var textToInsert = `test1
+test2
+test3`;
 
-(function() {
-    'use strict';
-    document.addEventListener('keydown', function(event) {
-        if (event.key === '\\' || event.key === 'Backslash') {
-            if (event.metaKey || event.ctrlKey) {
-                const textbox = document.createElement('textarea');
-                const button = document.createElement('button');
-                let userInput = prompt("What do you want your delay between characters to be in milliseconds? if no answer is inputted, will default to 0.1 second");
-                let delay23 = userInput ? parseFloat(userInput) : 100;
-                let userInput2 = prompt("What do you want your random break to be? if no answer is inputted, will default to 14 seconds");
-                let break23 = userInput2 ? parseFloat(userInput2) : 14000;
-                let textToInsert = ``;
+function insertTextWithDelay(text, delay, randomDelayChance, randomDelay) {
+    let index = 0;
 
-                textbox.style.position = 'fixed';
-                textbox.style.bottom = '50px';
-                textbox.style.left = '50%';
-                textbox.style.transform = 'translateX(-50%)';
-                textbox.style.width = '80%';
-                textbox.style.height = '100px';
-                textbox.style.zIndex = '10000';
+    if (window.textInterval) {
+        clearInterval(window.textInterval);
+    }
 
-                button.innerText = 'Enter';
-                button.style.position = 'fixed';
-                button.style.bottom = '10px';
-                button.style.left = '50%';
-                button.style.transform = 'translateX(-50%)';
-                button.style.width = '80%';
-                button.style.zIndex = '10000';
+    window.textInterval = setInterval(() => {
+        if (index < text.length) {
+            textarea.value += text.charAt(index);
+            index++;
 
-
-                document.body.appendChild(textbox);
-                document.body.appendChild(button);
-
-                const targetTextarea = document.getElementById('ace_text-input-textarea');
-                if (!targetTextarea) {
-                    console.error('Target textarea not found.');
+            if (text.charAt(index - 1) === '\n') {
+                if (Math.random() < randomDelayChance) {
+                    clearInterval(window.textInterval);
+                    window.textInterval = setTimeout(() => {
+                        insertTextWithDelay(text.substring(index), delay, randomDelayChance, randomDelay);
+                    }, randomDelay);
                     return;
                 }
-
-                button.addEventListener('click', () => {
-        if (targetTextarea) {
-            textToInsert = textbox.value;
-            const event = new Event('input', { bubbles: true });
-            targetTextarea.dispatchEvent(event);
-
-            textbox.style.display = 'none';
-            button.style.display = 'none';
-
-            executeCustomScript();
-        }
-                });
-
-                function executeCustomScript() {
-                    function insertTextWithDelay(text, delay, randomDelayChance, randomDelay) {
-                        let index = 0;
-
-                        if (window.textInterval) {
-                            clearInterval(window.textInterval);
-                        }
-
-                        window.textInterval = setInterval(() => {
-                            if (index < text.length) {
-                                targetTextarea.value += text.charAt(index);
-                                index++;
-
-                                if (text.charAt(index - 1) === '\n') {
-                                    if (Math.random() < randomDelayChance) {
-                                        clearInterval(window.textInterval);
-                                        window.textInterval = setTimeout(() => {
-                                            insertTextWithDelay(text.substring(index), delay, randomDelayChance, randomDelay);
-                                        }, randomDelay);
-                                        return;
-                                    }
-                                }
-
-                                const event = new Event('input', { bubbles: true });
-                                targetTextarea.dispatchEvent(event);
-                            } else {
-                                clearInterval(window.textInterval);
-                            }
-                        }, delay);
-                    }
-
-
-                    insertTextWithDelay(textToInsert, delay23, 0.10, break23);
-                }
-
-                event.preventDefault();
             }
+
+            var event = new Event('input', { bubbles: true });
+            textarea.dispatchEvent(event);
+        } else {
+            clearInterval(window.textInterval);
         }
-    });
+    }, delay);
+}
 
-
-})();
+insertTextWithDelay(textToInsert, 100, 0.10, 14000);
